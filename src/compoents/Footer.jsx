1c86e4ec80ea621eaf91c390179d6dbafcb7347f
Mohamedabdulla003 <abdullamohamed406@gmail.com>
2024-04-20 20:45:42 +0530

    first commit

---
+++ src/compoents/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <footer className="footer fixed-bottom bg-dark text-light p-3">
+                <div className="container bg-dark">
+                    <p className="text-center bg-dark mb-0"><span className="bg-dark">&copy; 2024 mohame abdulla s.All rights reserved.</span></p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
